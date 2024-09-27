d40b523307aab924759b8081561de1ac081cbd56
stefan.pinter <stefan.pinter@bearingpoint.com>
2024-09-27 14:11:14 +0200

    presentation mode fixes

--- aerospace/presentation.sh
+++ aerospace/presentation.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
 
 if [ $1 == "on" ]; then
-  sed -i 's/^outer.bottom.*/outer.bottom = 10/' ~/.config/aerospace/aerospace.toml
+  sed -i '' 's/^outer.bottom.*/outer.bottom = 300/' ~/.config/aerospace/aerospace.toml
+  sed -i '' "s/\(outer\.top = *\[.*\), 52]/\1, 300]/" ~/.config/aerospace/aerospace.toml
+  aerospace reload-config
 elif [ $1 == "off" ]; then
-  sed -i 's/^outer.bottom.*/outer.bottom = 300/' ~/.config/aerospace/aerospace.toml
+  sed -i '' 's/^outer.bottom.*/outer.bottom = 10/' ~/.config/aerospace/aerospace.toml
+  sed -i '' "s/\(outer\.top = *\[.*\), 300]/\1, 52]/" ~/.config/aerospace/aerospace.toml
+  aerospace reload-config
 fi
 
